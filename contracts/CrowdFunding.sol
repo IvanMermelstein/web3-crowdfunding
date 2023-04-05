98d767096e66cf9dd11cc218a5f27259752f2ebd
IvanMermelstein <ivan.mermelstein@gmail.com>
2023-04-05 10:24:20 -0300

    Last methods and config

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -61,7 +61,20 @@ contract CrowdFunding {
         }
     }
 
-    function getDonators() {}
+    function getDonators(
+        uint256 _id
+    ) public view returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
 
-    function getCampaigns() {}
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for (uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
 }
